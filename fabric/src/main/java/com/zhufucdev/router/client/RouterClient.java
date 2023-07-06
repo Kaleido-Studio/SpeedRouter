565ac19211a18554a3e3cbd46744e0f24edd0d4f
zhufucdev <1095291434@qq.com>
2023-07-07 07:52:33 +0800

    :tada: Add Fabric module

---
+++ fabric/src/main/java/com/zhufucdev/router/client/RouterClient.java
@@ -0,0 +1,10 @@
+package com.zhufucdev.router.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RouterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
