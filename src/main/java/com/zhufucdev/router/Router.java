806a0f6b1b0c5a1846948f90a0b78f12c1d4f5b3
zhufucdev <1095291434@qq.com>
2023-07-06 17:17:07 +0800

    :arrow_down: Downgrade to velocity 3.1.1

--- src/main/java/com/zhufucdev/router/Router.java
+++ src/main/java/com/zhufucdev/router/Router.java
@@ -1,22 +1,32 @@
 package com.zhufucdev.router;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.lifecycle.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import net.kyori.adventure.text.Component;
 import org.slf4j.Logger;
 
 @Plugin(
         id = "router",
         name = "router",
+        authors = {"zhufucdev"},
+        description = "A custom router",
         version = BuildConstants.VERSION
 )
 public class Router {
+    private final Logger logger;
+    private final ProxyServer server;
 
     @Inject
-    private Logger logger;
+    public Router(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+
     }
 }
